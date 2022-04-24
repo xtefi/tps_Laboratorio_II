97326aaf24af08a1fd9a622e26e05fe42fd61dfb
xtefi <tefa031tefa@hotmail.com>
2022-04-24 15:03:20 -0300

    tp2 up

---
+++ TP2/Entidades/Suv.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv: Vehiculo
+    {
+        #region CONSTRUCTORES
+        /// <summary>
+        /// Constructor de tipo Suv
+        /// </summary>
+        /// <param name="marca">Se asigna una marca de las disponibles en base</param>
+        /// <param name="chasis">para SUV chasis es tamaño "grande"</param>
+        /// <param name="color"> color del vehículo</param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        #endregion
+
+        #region PROPIEDADES
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+        #endregion
+
+        #region MÉTODOS
+        /// <summary>
+        /// Publica todos los datos de tipo Suv.
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
