74e4ebcda4d9b5785b0b38429a3726ee6c8eec6b
xtefi <tefa031tefa@hotmail.com>
2022-07-31 22:03:41 -0300

    TP3

---
+++ RecuperatoriosTP/TP3/Entidades/Personas/Transportista.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades.Enumerados;
+
+namespace Entidades
+{
+    public class Transportista
+    {
+        private string cuit;
+        private string patente;
+        private string nombre;
+        private long toneladas;
+        private Granos.Grano tipoGrano;
+        private DateTime fechaIngreso;
+        private long precioTonelada;
+        private DateTime fechaDescarga;
+
+        public string Nombre { get { return this.nombre; } set { this.nombre = value; } }
+        public string Patente { get { return this.patente; } }
+        public string Cuit { get { return this.cuit; } }
+        public DateTime FechaIngreso { get { return this.fechaIngreso; } }
+        public long Toneladas { get { return this.toneladas; } set { this.toneladas = value; } }
+        public Granos.Grano TipoGrano { get { return this.tipoGrano; } }
+        public long PrecioTonelada { 
+            get { return this.precioTonelada; } 
+            set { this.precioTonelada = value; } 
+        }
+        public DateTime FechaDescarga { get { return this.fechaDescarga; } set { this.fechaDescarga = value; } }
+
+        public Transportista()
+        {
+
+        }
+        public Transportista(string cuit, string nombre, string patente, long toneladas, Granos.Grano tipoGrano, long precioTonelada)
+        {
+            this.cuit = cuit;
+            this.nombre = nombre;
+            this.patente = patente;
+            this.toneladas = toneladas;
+            this.tipoGrano = tipoGrano;
+            this.PrecioTonelada = precioTonelada;
+            this.fechaIngreso = DateTime.Now;
+            this.fechaDescarga = DateTime.MinValue;
+        }
+
+        public long FacturarCamion(Granos.Grano tipoGrano, long precioTonelada)
+        {
+            long valor=0;
+            try
+            {
+                switch (tipoGrano)
+                {
+                    case Granos.Grano.Girasol:
+                        valor = toneladas * precioTonelada;
+                        break;
+                    case Granos.Grano.Maíz:
+                        valor = toneladas * precioTonelada;
+                        break;
+                    case Granos.Grano.Soja:
+                        valor = toneladas * precioTonelada;
+                        break;
+                    case Granos.Grano.Trigo:
+                        valor = toneladas * precioTonelada;
+                        break;
+                }
+            }
+            catch
+            {
+
+            }
+            return valor;
+        }
+        /*
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre: {this.Nombre}");
+            sb.AppendLine($"Cuil: {this.Cuit}");
+            sb.AppendLine($"Toneladas: {this.Toneladas}");
+            sb.AppendLine($"Tipo grano: {this.TipoGrano}");
+            return sb.ToString();
+        }*/
+    }
+}
---
+++ RecuperatoriosTP/TP3/Entidades/Serializadores/Serializador.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Entidades
+{
+    public class Serializador<T> : IArchivos<T> where T: class
+    {
+        private IArchivos<T>.ETipoArchivo tipo;
+
+        public Serializador(IArchivos<T>.ETipoArchivo tipo)
+        {
+            this.tipo = tipo;
+        }
+
+        public void Escribir(T dato, string path, bool pisarDatos)
+        {
+            try
+            {
+                if(this.tipo == IArchivos<T>.ETipoArchivo.XML)
+                {
+                    if(Path.GetExtension(path) == ".xml")
+                    {
+                        using (XmlTextWriter xmlTextW = new XmlTextWriter(path, Encoding.UTF8))
+                        {
+                            xmlTextW.Formatting = Formatting.Indented;
+                            XmlSerializer xmlSerial = new XmlSerializer(typeof(T));
+                            xmlSerial.Serialize(xmlTextW, dato);
+                        }
+                    }
+                    else
+                    {
+                        throw new ExtensionInvalidaException("Extensión inválida para XML");
+                    }
+                }
+                else
+                {
+                    if(Path.GetExtension(path) == ".json")
+                    {
+                        Archivador archivador = new Archivador();
+                        archivador.Escribir1(JsonSerializer.Serialize(dato, typeof(T)), path, pisarDatos);
+                    }
+                    else
+                    {
+                        throw new ExtensionInvalidaException("Extensión inválida para JSON");
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        public T Leer(string path)
+        {
+            try
+            {
+                if(this.tipo == IArchivos<T>.ETipoArchivo.XML)
+                {
+                    if(Path.GetExtension(path) == ".xml")
+                    {
+                        using(XmlTextReader xmlReader = new XmlTextReader(path))
+                        {
+                            XmlSerializer xmlSerial = new XmlSerializer(typeof(T));
+                            return (T)xmlSerial.Deserialize(xmlReader);
+                        }
+                    }
+                    else
+                    {
+                        throw new ExtensionInvalidaException("Extensión inválida para XML");
+                    }
+                }
+                else
+                {
+                    if(Path.GetExtension(path) == ".json")
+                    {
+                        Archivador archivador = new Archivador();
+                        return JsonSerializer.Deserialize<T>(archivador.Leer1(path));
+                    }
+                    else
+                    {
+                        throw new ExtensionInvalidaException("Extensión inválida para JSON");
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }       
+    }
+}
